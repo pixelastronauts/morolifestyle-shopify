c83d5c5b0c63f795a7c7248d70d1863428b80e83
Max Baan <max@pixelastronauts.nl>
2024-06-04 14:15:15 +0200

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,27 @@ module.exports = {
       fontFamily: {
         heading: 'var(--font-heading-family)',
       },
+      colors: {
+        moro: {
+          100: "#e3d3d4",
+          200: "#c8a7a9",
+          300: "#ac7b7f",
+          400: "#914f54",
+          500: "#752329",
+          600: "#5e1c21",
+          700: "#461519",
+          800: "#2f0e10",
+          900: "#170708"
+        },
+      },
+      transitionDuration: {
+        800: "800ms",
+      },
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
+        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
+        expo: "cubic-bezier(.55,.085,0,.99)",
+      },
     },
   },
   plugins: [],
